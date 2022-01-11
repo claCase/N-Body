07661270f560fa052d977fc8bdbb53a53e06208f
Claudio_Casellato <claudiocasellatoml@gmail.com>
2022-01-11 02:10:47 +0100

    circular animation generation

--- README.md
+++ README.md
@@ -4,8 +4,12 @@ N-Body System Simulation [Wiki Article](https://en.wikipedia.org/wiki/N-body_pro
 ![alt-text](https://wikimedia.org/api/rest_v1/media/math/render/svg/f0c5aab28749b00eb610136b76689a0f6cf57976) 
 <br /><br /> Position Update Equation: <br /><br />
 ![alt-text](https://github.com/claCase/N-Body/blob/main/Animations/equation.svg)
+<br/><br/> Generate animation parameters: <br/> 
+```console
+python3 .\main.py --initial "circle" --n_planets 10 --t 2500 --dt 0.02 --trace --trace_len 1700 --save 
+```
+![alt-text](https://github.com/claCase/N-Body/blob/main/Animations/animation9.gif)
 ![alt-text](https://github.com/claCase/N-Body/blob/main/Animations/animation8.gif)
-![alt-text](https://github.com/claCase/N-Body/blob/main/Animations/animation5.gif)
 ![alt-text](https://github.com/claCase/N-Body/blob/main/Animations/animation7.gif)
 ![alt text](https://github.com/claCase/N-Body/blob/main/Animations/animation.gif)
 ![alt-text](https://github.com/claCase/N-Body/blob/main/Animations/animation2.gif)
--- main.py
+++ main.py
@@ -64,32 +64,48 @@ class Simulator:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--uniform", action="store_true")
+    parser.add_argument("--initial", type=str, default="uniform")
     parser.add_argument("--n_planets", type=int, default=10)
     parser.add_argument("--t", type=int, default=600)
-    parser.add_argument("--dt", type=float, default=0.2)
+    parser.add_argument("--dt", type=float, default=0.02)
     parser.add_argument("--save", action="store_true")
     parser.add_argument("--trace", action="store_true")
+    parser.add_argument("--trace_len", type=int, default=60)
+    parser.add_argument("--skip", type=int, default=10)
     args = parser.parse_args()
     n_planets = args.n_planets
     t = args.t
     dt = args.dt
-    uniform = args.uniform
+    initial = args.initial
     save = args.save
     trace = args.trace
+    trace_len = args.trace_len
+    skip = args.skip
 
-    if uniform:
+    if initial == "uniform":
         planets_position = np.random.uniform(-100, 100, size=(n_planets, 2))
         planets_mass = np.random.uniform(5, 20, size=(n_planets,))
         initial_forces = np.random.uniform(-10, 10, size=(n_planets, 2))
+
+    elif initial == "circle":
+        theta = (np.linspace(0, 2 * np.math.pi, n_planets + 1)[:-1]).reshape(-1, 1)
+        radious = 30
+        x = np.cos(theta) * radious
+        y = np.sin(theta) * radious
+        force_scale = 25
+        dx = -np.sin(theta) * force_scale
+        dy = np.cos(theta) * force_scale
+        planets_position = np.concatenate([x, y], 1)
+        planets_mass = np.tile([10], n_planets)
+        initial_forces = np.concatenate([dx, dy], 1)
     else:
         n_planets = 2
         planets_position = np.asarray([[-20, -20], [-20, 0]])
         planets_mass = np.asarray([10] * n_planets)
-        #initial_forces = np.zeros((n_planets, 2))
-        initial_forces = np.asarray([[2,0], [5, 5]])
+        # initial_forces = np.zeros((n_planets, 2))
+        initial_forces = np.asarray([[2, 0], [5, 5]])
 
-    planets_mass = np.append(15, planets_mass)
+    planets_mass = np.append(25, planets_mass)
     initial_forces = np.concatenate([[[0, 0]], initial_forces], 0)
     planets_position = np.concatenate([[[0, 0]], planets_position], 0)
     names = np.arange(n_planets + 1)
@@ -104,14 +120,20 @@ if __name__ == "__main__":
     trajectories = np.asarray(trajectories)
     trajectories = np.swapaxes(trajectories, 0, 1)  # TxNxd
 
-    if uniform:
+    if initial == "uniform":
         max_lim = (160, 160)
         min_lim = (-160, -160)
+    elif initial == "circle":
+        max_lim = (40, 40)
+        min_lim = (-40, -40)
     else:
         max_lim = (90, 90)
         min_lim = (-40, -40)
-    counter = np.arange(t)
-    counter = np.minimum(counter, 60)
+
+    trajectories = trajectories[::skip]
+    counter = np.arange(trajectories.shape[0])
+    counter = np.minimum(counter, trace_len)
+
 
     def update(i, counter):
         plt.cla()
@@ -119,17 +141,17 @@ if __name__ == "__main__":
         for j, p in enumerate(planets):
             plt.scatter(trajectories[i, j, 0], trajectories[i, j, 1], s=np.maximum(p.mass, 50))
             if trace:
-                plt.plot(trajectories[i-counter[i]:i, j, 0], trajectories[i-counter[i]:i, j, 1])
+                plt.plot(trajectories[i - counter[i]:i, j, 0], trajectories[i - counter[i]:i, j, 1])
             plt.xlim(min_lim[0], max_lim[0])
             plt.ylim(min_lim[1], max_lim[1])
 
 
     fig = plt.figure()
-    animation = anim.FuncAnimation(fig, update, fargs=(counter,), frames=t, repeat=True, interval=1)
+    animation = anim.FuncAnimation(fig, update, fargs=(counter,), frames=trajectories.shape[0], repeat=True, interval=1)
     if save:
         print("Saving Animation")
         animation.save(
-            "./Animations/animation8.gif",
+            "./Animations/animation9.gif",
             writer="pillow",
         )
 
